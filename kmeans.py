b5943a16da752b3fd88d4a3a6cf7e393666ce335
Kamal Hasan Mahmud <kamalhm1997@gmail.com>
2018-04-03 07:57:27 +0700

    part 1

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.cluster import KMeans
 
-dataTrain = pd.read_csv('TrainingSet.txt', delimiter='\t')
+dataTrain = pd.read_csv('TrainingSet.txt', delimiter='\t').as_matrix()
 print(dataTrain)
+
+plt.scatter(dataTrain[:,0],dataTrain[:,1]) # dataTrain[:,0] mengambil kolom X, sedangkan [:,1] mengambil kolom Y
+# plt.title('Scatter Plot')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.show()
+kmeans = KMeans.fit()
